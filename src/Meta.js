8b3328ea6e5540fd722a1e1426ab6933cfb3e68a
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2019-05-31 15:44:21 +0200

    Added meta

---
+++ src/Meta.js
@@ -0,0 +1,48 @@
+import deepEquals from 'fast-deep-equal';
+
+class Meta {
+  constructor(base) {
+    this.data = base == null ? {} : base;
+  }
+
+  clear() {
+    this.data = {};
+  }
+
+  delete(key) {
+    const value = this.data[key];
+    delete this.data[key];
+    return value;
+  }
+
+  entries() {
+    return Object.entries(this.data);
+  }
+
+  get(key) {
+    return this.data[key];
+  }
+
+  has(key) {
+    return Object.prototype.hasOwnProperty.call(this.data, key);
+  }
+
+  keys() {
+    return Object.keys(this.data);
+  }
+
+  set(key, value) {
+    this.data[key] = value;
+  }
+
+  values() {
+    return Object.values(this.data);
+  }
+
+  commit(prev) {
+    if (prev != null && deepEquals(prev, this.data)) return prev;
+    return { ...this.data };
+  }
+}
+
+export default Meta;
