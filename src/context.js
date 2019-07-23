0fb14cfc801fc584e84b9cbcba562cdf3d7684b1
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2019-07-23 08:57:23 +0200

    added context hooks

--- src/context.js
+++ src/context.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 const noop = () => {};
 
 export const ConfigContext = React.createContext({});
+export const useConfigContext = () => useContext(ConfigContext);
+
 export const ResourceContext = React.createContext({ data: [], notify: noop });
+export const useResourceContext = () => useContext(ResourceContext);
