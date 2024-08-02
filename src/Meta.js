ff48dbe3cc81a9906ccd094e29b3b6567ea79ab2
Aaron <4709780+2manyvcos@users.noreply.github.com>
2024-08-02 15:53:59 +0200

    Performance optimization + Fix #7

--- src/Meta.js
+++ src/Meta.js
@@ -43,7 +43,7 @@ class Meta {
 
   commit(prev) {
     if (prev != null && deepEquals(prev, this.data)) return prev;
-    return { ...this.data };
+    return JSON.parse(JSON.stringify(prev));
   }
 }
 
