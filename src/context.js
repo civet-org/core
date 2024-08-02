ded0b110297c2f5fca362919bcd6bf6b819604ce
Aaron <4709780+2manyvcos@users.noreply.github.com>
2024-08-02 15:53:59 +0200

    Closes #6

--- src/context.js
+++ src/context.js
@@ -3,7 +3,9 @@ import React, { useContext } from 'react';
 const noop = () => {};
 
 export const ConfigContext = React.createContext({});
+ConfigContext.displayName = 'ConfigContext';
 export const useConfigContext = () => useContext(ConfigContext);
 
 export const ResourceContext = React.createContext({ data: [], notify: noop });
+ResourceContext.displayName = 'ResourceContext';
 export const useResourceContext = () => useContext(ResourceContext);
